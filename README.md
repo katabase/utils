e55ff6545cbf063e9c1f6ce37961705162392135
paul k <paul.kervegan@hotmail.fr>
2022-04-13 14:21:34 +0200

    13.04.22 - autopipeline.sh done

--- README.md
+++ README.md
@@ -3,10 +3,38 @@
 ---
 
 ## Description of the tools
-- `reorder.sh` : a bash script to move XML catalogues (`CAT_*.xml`) in the proper directories (`1-101`, `200-201`...) based on their id. 
+**`autopipeline.sh`** : a bash script to run the whole pipeline. The script checks if the directories used as inputs and outputs exists ; if
+output directories exist, a message informs the user that data might be overwritten and offers to delete those directories. Be careful not
+to delete anything too important, however.
+- **how to use it**
+	```bash
+	mkdir katabase  # create a folder to contain all repositories
+	cd katabase  # move in the proper directory
+	# clone all necessary repositories
+	git clone https://github.com/katabase/utils
+	git clone https://github.com/katabase/1_OutputData.git
+	git clone https://github.com/katabase/2_CleanedData.git
+	git clone https://github.com/katabase/3_TaggedData.git
+	cd utils  # move in the utils directory
+	bash autopipeline.sh  # launch the script
+	```
+
+**`reorder.sh`** : a bash script to move XML catalogues (`CAT_*.xml`) in the proper directories (`1-101`, `200-201`...) based on their id. 
 The script is supposed to be usable in all steps of the Katabase pipeline
-	- example : `CAT_000176.xml` will be moved in a directory named `1-100` and so on. 
-	- he script checks if the destination directory exists ; if not, it creates it and moves the file there. 
-	- it also the location of all `CAT_*.xml` files and moves them to the proper directory if needed.
-- `jsontocsv.py` : a python script to transform `export.json` (the json file obtained at the end of step `3_TaggedData`) in CSV format. `export.json` 
+- *example* : `CAT_000176.xml` will be moved in a directory named `1-100` and so on.
+- the script checks if the destination directory exists ; if not, it creates it and moves the file there. 
+- it also the location of all `CAT_*.xml` files and moves them to the proper directory if needed.
+- **how to**
+	```shell
+	cp utils/reorder.sh 1_OutputData  # copy the script to the directory you want to use it in (1_OutputData, 2_CleanData, 3_TaggedData)
+	cd 1_OutputData  # move in the directory you'll be using the script in
+	bash reorder.sh
+	```
+
+**`jsontocsv.py`** : a python script to transform `export.json` (the json file obtained at the end of step `3_TaggedData`) in CSV format. `export.json` 
 needs to be in the same folder as this script to work.
+- **how to**
+        ```shell 
+        # have `jsontocsv.py` and `export.json` in the same directory
+        python jsontocsv.py
+	```
--- autopipeline.sh
+++ autopipeline.sh
@@ -1,44 +1,134 @@
 #!/bin/bash
 
-root="$(readlink -e ..)"  # root / parent directory
-rgx="^([0-9]+-[0-9]+)(_clean|_tagged)?/?$"
 
-# étapes de vérification ; trucs à vérifier
-# - qu'il y a des dossiers *0*/ dans 1_OutputData
-# - qu'il n'y a de dossier output nulle part
-# - qu'il n'y a pas de *0*(_clean|_tagged)/ dans les dépôts 2 et 3
+# bash script to run the whole pipeline (1_OutputData, 2_CleanedData, 3_TaggedData)
+#
+# HOW TO USE :
+# - all the directories used must be at the root of the same folder (named $root afterwards)
+# - the current file must be in a child of $root (the utils directory, for example)
+# - 1_OutputData must contain the input files, at the root of that directory ; they
+#   must match the pattern bash *0*
+# - files in all the directories are matched using bash or regex patterns ;
+#   when launching this script, the script checks which files could be overwritten
+#   (the files that match the bash and regex patterns) ; the script offers you the
+#   option to erase the files, or to interrupt the script in order to check the files
+#   manually. BE SMART: directories that would be deleted will be listed ; if you see
+#   anything you want to save, make backups.
+#
+# *basically* :
+# - clone / pull all necessary repositories in a directory
+# - `cd utils`
+# - `bash autopipeline.sh`
+#
+# EXPECTED DIRECTORY STRUCTURE
+# the script only works in a directory as follows (with extra files, obviously) :
+# |_utils/
+# |   |_autopipeline.sh
+# |_1_OutputData/
+# |   |_*0*/ directories (1-100, 101-200...)
+# |   |_script/
+# |       |_clean_xml.py (the first script to be ran)
+# |_2_CleanedData/
+# |   |_script/
+# |       |_extractor_xml.py
+# |_3_TaggedData/
+# |   |_script/
+# |       |_extractor_json.py
+
+
+root="$(readlink -e ..)"  # root / parent directory
+rgx="^([0-9]+-[0-9]+)(_clean|_tagged)?/?$"  # regex to match the directories
+paths=($root/1_OutputData/output $root/2_CleanedData/*0* $root/2_CleanedData/output $root/3_TaggedData/Catalogues/*0* $root/3_TaggedData/output)  # output directories
+paths_todel=()  # array of output paths to delete in order to run the script
+
+
+# ----- check if outputs aldeady exist ; if so, offer to delete them ----- #
+# check if outputs aldeady exist ; if so, add them to $paths_todel
+for path in "${paths[@]}" ; do
+  if [[ -d $path ]] ; then
+    echo "* output directory $path aldready exists and could be erased by the script *"
+    paths_todel+=("${path}")
+  fi
+done
+# offer to delete them ; if they don't get deleted, the script is ended and the paths should be
+# deleted by hand
+if (( ${#paths_todel[@]} > 0 )) ; then  # "#paths_todel[@]" : length of the array
+  echo ""
+  echo "* ${#paths_todel[@]} path(s) aldready exist and would be overwritten by the script. Would you like to delete them [Y/N] ? *
+    * (WARNING : please check what is going to be deleted to avoid loosing important files) *"
+  read input
+  echo ""
+  if [[ $input == "Y" ]] ; then
+    for path in "${paths_todel[@]}" ; do
+      rm -r $path
+      echo "* $path removed. *"
+    done
+    echo ""
+  else
+    echo "You have chosen not to delete the paths. Please check and delete them by hand before running the script."
+    exit 1
+  fi
+fi
+
 
 # ----- STEP 1_OutputData ----- #
 echo "* Beginning step 1_OutputData *"
 
 # launching the python script
 cd $root/1_OutputData
-for dir in *0*/ ; do
-  echo "* working with directory $dir *"
-  python script/clean_xml.py -d $dir
-done
+if (( $(ls -d -1q *0* | wc -l) > 0 )) ; then  # if directories match the input pattern in 1_OutputData, run the script ; else, exit
+  for dir in *0*/ ; do
+    echo "* working with directory $dir *"
+    python script/clean_xml.py -d $dir
+  done
+else
+  echo "* No directories matching the pattern *0*. Exiting the script... *"
+  exit 1
+fi
 echo "* Step 1 done ! *"
+echo ""
+
 
 # ----- moving the files from step 1 to 2 ----- #
-# moving the files to 2_CleanedData and moving back to $root
+# moving the files to 2_CleanedData and changing directory to 2_CLeanedData
 cp -r output/*0*_clean/ $root/2_CleanedData
-cd $root
 cd $root/2_CleanedData
 # renaming the files properly
 for dir in *0*_clean ; do
   if [[ $dir =~ $rgx ]] ; then
     mv $dir ${BASH_REMATCH[1]}
   else
-    echo "* directory $dir name does not match pattern \$rgx *"
+    echo "* directory $dir name does not match pattern $rgx *"  # improbable
   fi
 done
 
+
 # ----- STEP 2_CleanedData ----- #
-echo "Beginning step 2_CleanedData"
+echo "* Beginning step 2_CleanedData *"
 for dir in *0* ; do
   echo "* working with directory $dir *"
   cd ./script && python extractor_xml.py ../$dir && cd ..
 done
 echo "* Step 2 done ! *"
+echo ""
+
 
 # ----- moving the files from step 2 to 3 ----- #
+# moving the files to 3_TaggedData and changing directory to 3_TaggedData
+cp -r output/*0*_tagged/ $root/3_TaggedData/Catalogues
+cd $root/3_TaggedData/Catalogues
+# renaming the files properly
+for dir in *0* ; do
+  if [[ $dir =~ $rgx ]] ; then
+    mv $dir ${BASH_REMATCH[1]}
+  else
+    echo "* directory $dir name does not match pattern $rgx *"  # improbable
+  fi
+done
+
+
+# ----- STEP 3_TaggedData ----- #
+echo "* Beginning step 3_TaggedData *"
+cd $root/3_TaggedData/script
+python3 extractor_json.py
+echo "* Step 3 done ! *"
